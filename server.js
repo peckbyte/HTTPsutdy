727bbef4120f8653b47fbf9f8549e5c0d9f475ac
peckbyte <503410224@qq.com>
2018-08-28 22:18:12 +0800

    3.4 Cache-Control 代码测试完成

--- server.js
+++ server.js
@@ -2,11 +2,21 @@ const http = require('http')
 const fs = require('fs')
 http.createServer(function (request, response) {
     console.log('request come', request)
-    const html = fs.readFileSync('test.html', 'utf8')
+    if (request.url === '/') {
+        const html = fs.readFileSync('test.html', 'utf8')
+        response.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        response.end(html)
+    }
+
+    if (request.url === '/script.js') {
     response.writeHead(200, {
-        'Content-Type': 'text/html'
+        'Content-Type': 'text/javascript',
+        'Cache-Control': 'max-age=200',
     })
-    response.end(html)
+    response.end('console.log("script load")')
+}
 }).listen(8888)
 
 console.log('server listening 88888')
