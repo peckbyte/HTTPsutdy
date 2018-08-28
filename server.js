84e903a69367e3a5ad2f3f345d7924dba85e62d2
peckbyte <503410224@qq.com>
2018-08-28 19:00:48 +0800

    增加test.html server.js的Content-Type 为text/html

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const http = require('http')
-
+const fs = require('fs')
 http.createServer(function (request, response) {
     console.log('request come', request)
-
-    response.end('124')
+    const html = fs.readFileSync('test.html', 'utf8')
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    })
+    response.end(html)
 }).listen(8888)
 
 console.log('server listening 88888')
