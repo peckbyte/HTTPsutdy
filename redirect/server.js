9d55250f1d90106f4e74d1dfaa2145a0ced22ef3
peckbyte <503410224@qq.com>
2018-08-29 22:09:04 +0800

    3.9 redirect 代码完成，测试成功

---
+++ redirect/server.js
@@ -0,0 +1,16 @@
+const http = require('http')
+const fs = require('fs')
+const zlib = require('zlib')
+
+http.createServer(function (request, response) {
+    console.log('request come', request)
+
+    const html = fs.readFileSync('test.html')
+            response.writeHead(200, {
+                'Content-Type': 'text/html',
+                'Content-Encoding': 'gzip'
+            })
+        response.end(zlib.gzipSync(html))
+}).listen(8888)
+
+console.log('server listening 88888')
